bfdefdcfd901afaa9ef853c332ddf13f6c2a3b85
tyronmaster <degonski.art@gmail.com>
2022-02-08 13:51:52 +0200

    feat: add base layout

---
+++ script.js
@@ -0,0 +1,42 @@
+var url = "http://www.omdbapi.com/?apikey=c8ed38c2&t=game&";
+
+fetch(url, {
+   "method": "GET",
+})
+   .then(response => console.log(response.json()))
+   /*.then(data => {
+      console.log(data);
+      const list = data.Poster;
+      console.log(list);
+   })*/
+   .catch(err => {
+      console.error(err);
+   });
+
+
+/* IMDB API */
+/*
+fetch("https://imdb8.p.rapidapi.com/auto-complete?q=game", {
+   "method": "GET",
+   "headers": {
+      "x-rapidapi-host": "imdb8.p.rapidapi.com",
+      "x-rapidapi-key": "5034c4df4emsh426a86961656757p1cf604jsn169cd90a84a4"
+   }
+})
+   .then(response => response.json())
+   .then(data => {
+      //console.log(data);
+      const list = data.d;
+
+      list.map( (item) => {
+         const name = item.l;
+         const poster = item.i.imageUrl;
+         const movie = `<li><img src="${poster}" alt="image poster"><h2>${name}</h2></li>`;
+         document.querySelector(".movies").innerHTML += movie;
+      })
+      //console.log(list);
+   })
+   .catch(err => {
+      console.error(err);
+   });
+   */
